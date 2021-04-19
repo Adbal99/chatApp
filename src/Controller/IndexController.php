05ae440a6e3ee80f0ccf601fd2a11be24e9e82da
AdrianBaz <adrian.bazela@triverna.pl>
2021-04-19 15:36:04 +0200

    added token cookie in index controller

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\Signer\Key\InMemory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +16,39 @@ class IndexController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('index/index.html.twig', [
+        $username = $this->getUser()->getUsername();
+
+        $config = Configuration::forSymmetricSigner(new Sha256(), InMemory::plainText($this->getParameter('mercure_secret_key')));
+        // lcobucci\jwt library
+        $token = $config->builder()
+            ->withClaim('mercure', ['subscribe' => [sprintf("/%s", $username)]])
+            ->getToken(
+                $config->signer(),
+                $config->signingKey()
+            );
+
+        $response = $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
+
+
+        $response->headers->setCookie(
+            new Cookie(
+                'mercureAuthorization',
+                $token->toString(),
+                (new \DateTime())
+                    ->add(new \DateInterval('PT2H')),
+                '/.well-known/mercure',
+                null,
+                false,
+                true,
+                false,
+                'strict'
+            )
+        );
+
+    
+
+        return $response;
     }
 }
